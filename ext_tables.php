be9bf571fd60647e6c0b917946173fe1da72c038
Christian Fries <hallo@christian-fries.ch>
2016-12-06 22:35:11 +0100

    [TASK] Add icon for backend module

--- ext_tables.php
+++ ext_tables.php
@@ -23,7 +23,7 @@ call_user_func(
                     ],
                     [
                         'access' => 'user,group',
-                        'icon' => 'EXT:' . $extKey . '/ext_icon.gif',
+                        'icon'   => 'EXT:' . $extKey . '/Resources/Public/Icons/module-admin.png',
                         'labels' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_admin.xlf',
                         'navigationComponentId' => $navigationComponent,
                     ]
