76268ec91e6f8522f0a2eec0660ed97469c1a880
Christian Fries <christian.fries@lst.team>
2018-11-22 11:12:50 +0100

    [BUGFIX] PluginPreview not working in TYPO3 9

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,6 +10,4 @@ if (!defined('TYPO3_MODE')) {
  * Hook to show PluginInformation under a tt_content element in page module of type teaser_manager
  */
 $cmsLayout = 'cms/layout/class.tx_cms_layout.php';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][$cmsLayout]['tt_content_drawItem']['teaser_manager'] =
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('teaser_manager') .
-    'Classes/Hook/PluginPreview.php:CHF\TeaserManager\Hook\PluginPreview';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][$cmsLayout]['tt_content_drawItem']['teaser_manager'] = 'CHF\TeaserManager\Hook\PluginPreview';
