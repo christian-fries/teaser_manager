88c697938d94a90918c872bcafff63cbacd47af3
Christian Fries <hallo@christian-fries.ch>
2019-02-13 21:57:25 +0100

    [TASK] Add new lazy annotations

--- Classes/Domain/Model/Teaser.php
+++ Classes/Domain/Model/Teaser.php
@@ -13,6 +13,7 @@ namespace CHF\TeaserManager\Domain\Model;
  *
  ***/
 
+use TYPO3\CMS\Extbase\Annotation as Extbase;
 use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 class Teaser extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
@@ -69,12 +70,14 @@ class Teaser extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
 
     /**
      * @var \LST\People\Domain\Model\Person
+     * @Extbase\ORM\Lazy
      * @lazy
      */
     protected $person = null;
 
     /**
      * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\LST\People\Domain\Model\Person>
+     * @Extbase\ORM\Lazy
      * @lazy
      */
     protected $persons = null;
@@ -96,12 +99,14 @@ class Teaser extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
 
     /**
      * @var \CHF\ColorManager\Domain\Model\Color
+     * @Extbase\ORM\Lazy
      * @lazy
      */
     protected $color = null;
 
     /**
      * @var \CHF\TeaserManager\Domain\Model\TeaserType
+     * @Extbase\ORM\Lazy
      * @lazy
      */
     protected $type = null;
