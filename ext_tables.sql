b6a7914137893edbf5b62da70e9e833fabd4f63a
Christian Fries <hello@christian-fries.ch>
2020-04-20 15:57:47 +0200

    [TASK] Remove custom management DB field definitions

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,11 +1,4 @@
-#
-# Table structure for table 'tx_teasermanager_domain_model_teaser'
-#
 CREATE TABLE tx_teasermanager_domain_model_teaser (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	name varchar(255) DEFAULT '' NOT NULL,
 	title varchar(255) DEFAULT '' NOT NULL,
 	subtitle varchar(255) DEFAULT '' NOT NULL,
@@ -23,89 +16,24 @@ CREATE TABLE tx_teasermanager_domain_model_teaser (
 	images int(11) unsigned NOT NULL default '0',
 	size varchar(255) DEFAULT '' NOT NULL,
 	type varchar(255) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	t3ver_oid int(11) DEFAULT '0' NOT NULL,
-	t3ver_id int(11) DEFAULT '0' NOT NULL,
-	t3ver_wsid int(11) DEFAULT '0' NOT NULL,
-	t3ver_label varchar(255) DEFAULT '' NOT NULL,
-	t3ver_state tinyint(4) DEFAULT '0' NOT NULL,
-	t3ver_stage int(11) DEFAULT '0' NOT NULL,
-	t3ver_count int(11) DEFAULT '0' NOT NULL,
-	t3ver_tstamp int(11) DEFAULT '0' NOT NULL,
-	t3ver_move_id int(11) DEFAULT '0' NOT NULL,
-
-	sys_language_uid int(11) DEFAULT '0' NOT NULL,
-	l10n_parent int(11) DEFAULT '0' NOT NULL,
-	l10n_diffsource mediumblob,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid),
-	KEY t3ver_oid (t3ver_oid,t3ver_wsid),
-	KEY language (l10n_parent,sys_language_uid)
-
 );
 
-#
-# Table structure for table 'tx_teasermanager_domain_model_teasertype'
-#
 CREATE TABLE tx_teasermanager_domain_model_teasertype (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	title varchar(255) DEFAULT '' NOT NULL,
 	fields varchar(255) DEFAULT '' NOT NULL,
 	layouts int(11) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid)
 );
 
-#
-# Table structure for table 'tx_teasermanager_domain_model_teaserlayout'
-#
 CREATE TABLE tx_teasermanager_domain_model_teaserlayout (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	title varchar(255) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid)
 );
 
-
-#
-# Table structure for table 'tt_content'
-#
 CREATE TABLE tt_content (
-
 	teaser_type int(11) unsigned DEFAULT '0' NOT NULL,
 	teaser_layout int(11) unsigned DEFAULT '0' NOT NULL,
 	teaser_items int(11) unsigned DEFAULT '0' NOT NULL,
-
 );
 
-#
-# Table structure for table 'tx_teasermanager_ttcontent_teaser_mm'
-#
 CREATE TABLE tx_teasermanager_ttcontent_teaser_mm (
 	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
 	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
@@ -116,9 +44,6 @@ CREATE TABLE tx_teasermanager_ttcontent_teaser_mm (
 	KEY uid_foreign (uid_foreign)
 );
 
-#
-# Table structure for table 'tx_teasermanager_teasertype_teaserlayout_mm'
-#
 CREATE TABLE tx_teasermanager_teasertype_teaserlayout_mm (
 	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
 	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
@@ -129,9 +54,6 @@ CREATE TABLE tx_teasermanager_teasertype_teaserlayout_mm (
 	KEY uid_foreign (uid_foreign)
 );
 
-#
-# Table structure for table 'tx_teasermanager_teaser_person_mm'
-#
 CREATE TABLE tx_teasermanager_teaser_person_mm (
     uid_local int(11) DEFAULT '0' NOT NULL,
     uid_foreign int(11) DEFAULT '0' NOT NULL,
