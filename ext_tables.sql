f9d7cbaafef12add278d5a8c9c8e8828b8bb74ea
Tobias Kummer <tobias.kummer@lst.team>
2020-05-11 11:31:50 +0200

    [TASK] Adjust formatting of sql file

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,63 +1,70 @@
-CREATE TABLE tx_teasermanager_domain_model_teaser (
-	name varchar(255) DEFAULT '' NOT NULL,
-	title varchar(255) DEFAULT '' NOT NULL,
-	subtitle varchar(255) DEFAULT '' NOT NULL,
-	link_text varchar(255) DEFAULT '' NOT NULL,
-	link varchar(255) DEFAULT '' NOT NULL,
-	text text NOT NULL,
-	date int(11) DEFAULT '0' NOT NULL,
-	color int(11) unsigned DEFAULT '0' NOT NULL,
-	icon varchar(255) DEFAULT '' NOT NULL,
-	selected_icon varchar(255) DEFAULT '' NOT NULL,
-	style varchar(255) DEFAULT '' NOT NULL,
-	person int(11) unsigned DEFAULT '0' NOT NULL,
-	persons int(11) unsigned DEFAULT '0' NOT NULL,
-	image int(11) unsigned NOT NULL default '0',
-	images int(11) unsigned NOT NULL default '0',
-	size varchar(255) DEFAULT '' NOT NULL,
-	type varchar(255) DEFAULT '' NOT NULL,
+CREATE TABLE tx_teasermanager_domain_model_teaser
+(
+    name          varchar(255)     DEFAULT ''  NOT NULL,
+    title         varchar(255)     DEFAULT ''  NOT NULL,
+    subtitle      varchar(255)     DEFAULT ''  NOT NULL,
+    link_text     varchar(255)     DEFAULT ''  NOT NULL,
+    link          varchar(255)     DEFAULT ''  NOT NULL,
+    text          text,
+    date          int(11)          DEFAULT '0' NOT NULL,
+    color         int(11) unsigned DEFAULT '0' NOT NULL,
+    icon          varchar(255)     DEFAULT ''  NOT NULL,
+    selected_icon varchar(255)     DEFAULT ''  NOT NULL,
+    style         varchar(255)     DEFAULT ''  NOT NULL,
+    person        int(11) unsigned DEFAULT '0' NOT NULL,
+    persons       int(11) unsigned DEFAULT '0' NOT NULL,
+    image         int(11) unsigned DEFAULT '0' NOT NULL,
+    images        int(11) unsigned DEFAULT '0' NOT NULL,
+    size          varchar(255)     DEFAULT ''  NOT NULL,
+    type          varchar(255)     DEFAULT ''  NOT NULL
 );
 
-CREATE TABLE tx_teasermanager_domain_model_teasertype (
-	title varchar(255) DEFAULT '' NOT NULL,
-	fields varchar(255) DEFAULT '' NOT NULL,
-	layouts int(11) unsigned DEFAULT '0' NOT NULL,
+CREATE TABLE tx_teasermanager_domain_model_teasertype
+(
+    title   varchar(255)     DEFAULT ''  NOT NULL,
+    fields  varchar(255)     DEFAULT ''  NOT NULL,
+    layouts int(11) unsigned DEFAULT '0' NOT NULL
 );
 
-CREATE TABLE tx_teasermanager_domain_model_teaserlayout (
-	title varchar(255) DEFAULT '' NOT NULL,
+CREATE TABLE tx_teasermanager_domain_model_teaserlayout
+(
+    title varchar(255) DEFAULT '' NOT NULL
 );
 
-CREATE TABLE tt_content (
-	teaser_type int(11) unsigned DEFAULT '0' NOT NULL,
-	teaser_layout int(11) unsigned DEFAULT '0' NOT NULL,
-	teaser_items int(11) unsigned DEFAULT '0' NOT NULL,
+CREATE TABLE tt_content
+(
+    teaser_type   int(11) unsigned DEFAULT '0' NOT NULL,
+    teaser_layout int(11) unsigned DEFAULT '0' NOT NULL,
+    teaser_items  int(11) unsigned DEFAULT '0' NOT NULL
 );
 
-CREATE TABLE tx_teasermanager_ttcontent_teaser_mm (
-	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
-	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+CREATE TABLE tx_teasermanager_ttcontent_teaser_mm
+(
+    uid_local       int(11) unsigned DEFAULT '0' NOT NULL,
+    uid_foreign     int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting         int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 
-	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign)
+    KEY uid_local (uid_local),
+    KEY uid_foreign (uid_foreign)
 );
 
-CREATE TABLE tx_teasermanager_teasertype_teaserlayout_mm (
-	uid_local int(11) unsigned DEFAULT '0' NOT NULL,
-	uid_foreign int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting int(11) unsigned DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
+CREATE TABLE tx_teasermanager_teasertype_teaserlayout_mm
+(
+    uid_local       int(11) unsigned DEFAULT '0' NOT NULL,
+    uid_foreign     int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting         int(11) unsigned DEFAULT '0' NOT NULL,
+    sorting_foreign int(11) unsigned DEFAULT '0' NOT NULL,
 
-	KEY uid_local (uid_local),
-	KEY uid_foreign (uid_foreign)
+    KEY uid_local (uid_local),
+    KEY uid_foreign (uid_foreign)
 );
 
-CREATE TABLE tx_teasermanager_teaser_person_mm (
-    uid_local int(11) DEFAULT '0' NOT NULL,
+CREATE TABLE tx_teasermanager_teaser_person_mm
+(
+    uid_local   int(11) DEFAULT '0' NOT NULL,
     uid_foreign int(11) DEFAULT '0' NOT NULL,
-    sorting int(11) DEFAULT '0' NOT NULL,
+    sorting     int(11) DEFAULT '0' NOT NULL,
 
     KEY uid_local (uid_local),
     KEY uid_foreign (uid_foreign)
